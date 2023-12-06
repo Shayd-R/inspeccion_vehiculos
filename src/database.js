583a1f7f1442615a89e5936a30f3f7d93b6a7439
Shayd-R <shaydruano@gmail.com>
2023-12-05 21:23:36 -0500

    merge

--- src/database.js
+++ src/database.js
@@ -1,11 +1,15 @@
-import {createPool} from "mysql2/promise";
+import { createPool } from "mysql2/promise";
+import dotenv from "dotenv";
+
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config();
 
 const pool = createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'suratrans'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE
 });
 
 export default pool;
