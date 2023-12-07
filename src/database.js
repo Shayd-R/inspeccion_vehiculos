9168959c7f7def4e3676d88eb8e3db76952f7fdd
Shayd-R <shaydruano@gmail.com>
2023-12-07 13:41:45 -0500

    database

--- src/database.js
+++ src/database.js
@@ -7,7 +7,7 @@ const pool = createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD || '', // Utiliza una cadena vacía si DB_PASSWORD no está definido
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DATABASE
 });
 
